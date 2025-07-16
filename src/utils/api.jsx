efc5623d335e605ef332bfabc02b2dc6a3ee7a2e
Zack Edwin <zakayooshome254@gmail-com>
2025-07-16 23:32:11 +0300

    Fix: use production backend URL from env

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
